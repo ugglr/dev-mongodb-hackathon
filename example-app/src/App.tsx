b17721e2a2cfd292f7cdfc19f439c81a7172871d
Ugglr <carl.igelstrom@gmail.com>
2021-12-31 17:03:40 +0800

    events and charting works.

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -48,10 +48,31 @@ function App() {
           </button>
           <button
             className={styles.baseButton}
-            onClick={async () => await sendEvent('CTA1 clicked!')}
+            onClick={async () => await sendEvent('CTA3 clicked!')}
           >
             CTA3
           </button>
+        </div>
+        <h2 style={{ marginTop: '60px' }}>
+          The below part makes up the internal analytics charting
+        </h2>
+        <p>
+          By using mongoDB charts one can easily setup new dashboards for your
+          apps analytics.
+        </p>
+        <div className={styles.chartsContainer}>
+          <p>Refreshes every 10s</p>
+          <iframe
+            style={{
+              background: '#FFFFFF',
+              border: 'none',
+              borderRadius: '2px',
+              boxShadow: '0 2px 10px 0 rgba(70, 76, 79, .2)',
+            }}
+            width="640"
+            height="480"
+            src="https://charts.mongodb.com/charts-dev-to-hackathon-flbpa/embed/charts?id=92c1fae7-fff1-422d-bc65-756e3dff040a&maxDataAge=60&theme=light&autoRefresh=true"
+          ></iframe>
         </div>
       </div>
     </Layout>
